89336f1c976508446f8e92115577b496f143f13c
Aditya Harsh <adityasiwan@live.com>
2017-04-11 21:23:23 +0530

    Check: Travis Build

--- Chapter 1/test.py
+++ Chapter 1/test.py
@@ -1,35 +1,27 @@
-import unittest
+from LinkedList import LinkedList
 
 
-def unique(string):
-    # Assuming character set is ASCII (128 characters)
-    if len(string) > 128:
-        return False
+def partition(ll, x):
+    current = ll.tail = ll.head
 
-    char_set = [False for _ in range(128)]
-    for char in string:
-        val = ord(char)
-        if char_set[val]:
-            # Char already found in string
-            return False
-        char_set[val] = True
+    while current:
+        nextNode = current.next
+        current.next = None
+        if current.value < x:
+            current.next = ll.head
+            ll.head = current
+        else:
+            ll.tail.next = current
+            ll.tail = current
+        current = nextNode
 
-    return True
+    # Error check in case all nodes are less than x
+    if ll.tail.next is not None:
+        ll.tail.next = None
 
 
-class Test(unittest.TestCase):
-    dataT = [('abcd'), ('s4fad'), ('')]
-    dataF = [('23ds2'), ('hb 627jh=j ()')]
-
-    def test_unique(self):
-        # true check
-        for test_string in self.dataT:
-            actual = unique(test_string)
-            self.assertTrue(actual)
-        # false check
-        for test_string in self.dataF:
-            actual = unique(test_string)
-            self.assertFalse(actual)
-
-if __name__ == "__main__":
-unittest.main()
+ll = LinkedList()
+ll.generate(10, 0, 99)
+print(ll)
+partition(ll, ll.head.value)
+print(ll)
